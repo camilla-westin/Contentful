b69701eb39a48d6798624e2f3cd3321b031b33f1
Camilla Westin <camilla@webstin.se>
2024-06-25 13:04:51 +0200

    Extracted HeroPost to separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Link from "next/link";
 import { draftMode } from "next/headers";
-
-import Date from "./components/date";
-import CoverImage from "./components/cover-image";
-import Avatar from "./components/avatar";
 import MoreStories from "./components/more-stories";
-
+import HeroPost from "./components/heropost";
 import { getAllPosts } from "@/lib/api";
 
 function Intro() {
@@ -14,51 +9,6 @@ function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Dreamy Destinations.
       </h1>
-    </section>
-  );
-}
-
-function HeroPost({
-  title,
-  postImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: {
-  title: string;
-  postImage: any;
-  date: string;
-  excerpt: string;
-  author: any;
-  slug: string;
-}) {
-  return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage
-          title={postImage.imageTitle}
-          slug={slug}
-          url={postImage.image.url}
-          alttext={postImage.altText}
-        />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/posts/${slug}`} className="hover:underline">
-              {title}
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <Date dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {author && <Avatar name={author.name} picture={author.picture} />}
-        </div>
-      </div>
     </section>
   );
 }
