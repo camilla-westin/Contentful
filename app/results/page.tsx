45c4d8018167a2095da56a824c65ba9e5dc7def5
Camilla Westin <camilla@webstin.se>
2024-07-02 10:32:36 +0200

    Show result based on query parameters

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,14 +1,24 @@
 "use client";
+import { getAllHotels } from "@/lib/api";
 import { useSearchParams } from "next/navigation";
+import HotelCard from "../components/hotel-card";
 
-export default function ResultsPage() {
+export default async function ResultsPage() {
   const searchParams = useSearchParams();
   const selected = searchParams.get("selected");
+  const hotels = (await getAllHotels(false)) || [];
 
   return (
-    <div>
-      <h1>Results Page</h1>
+    <div className="container mx-auto px-5">
+      <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight py-4 md:pr-8">
+        Results Page
+      </h1>
       <p>Selected: {selected}</p>
+      {hotels
+        .filter((hotel) => hotel.country.title === selected)
+        .map((hotel) => (
+          <HotelCard hotel={hotel} key={hotel.id} />
+        ))}
     </div>
   );
 }
